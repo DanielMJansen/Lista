1dba6e236963558efb72c702de127134d071df86
informatica <informatica@vraja.blumenau.ifc.edu.br>
2017-09-15 10:25:37 -0300

    e nois

--- Lista/src/lista/Lista.java
+++ Lista/src/lista/Lista.java
@@ -1,11 +1,17 @@
-
 package lista;
 
-public class Lista {
-String nome;
-int id;
-    public static void main(String[] args) {
 
+public class Lista {
+
+    String nome;
+    int id;
+
+    public static void main(String[] args) {
+    static Serie
+
+    {
+        String nome;
+        int id;
     }
-    
+    }
 }
